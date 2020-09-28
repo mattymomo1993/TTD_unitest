23daed13573e705623e94017ae1947b813b14aa7
Matthew Morris <matthewjmorris1993@gmail.com>
2020-09-28 16:52:19 -0400

    JT helped me understand the way to test

--- echo.py
+++ echo.py
@@ -2,22 +2,46 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "???"
+__author__ = "Matthew Morris"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.'
+    )
+    parser.add_argument('text', help='text to be manipulated')
+    parser.add_argument(
+        '-u', '--upper', help='convert text to uppercase',
+        action='store_true'
+    )
+    parser.add_argument(
+        '-l', '--lower', help='convert text to lowercase',
+        action='store_true'
+    )
+    parser.add_argument(
+        '-t', '--title', help='convert text to titlecase',
+        action='store_true'
+    )
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+    parser = create_parser()
+    ns = parser.parse_args(args)
+    text = ns.text
+    if ns.upper:
+        text = text.upper()
+    if ns.lower:
+        text = text.lower()
+    if ns.title:
+        text = text.title()
+    print(text)
 
 
 if __name__ == '__main__':
